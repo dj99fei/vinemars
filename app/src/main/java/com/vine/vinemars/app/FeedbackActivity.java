0b7f76e4aad87807f2cbd85bdd62c3767451cd88
Fenn.Cheng <dj99fei@gmail.com>
2014-12-06 23:04:58 +0800

    add feedback construction

---
+++ app/src/main/java/com/vine/vinemars/app/FeedbackActivity.java
@@ -0,0 +1,15 @@
+package com.vine.vinemars.app;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by chengfei on 14/12/6.
+ */
+public class FeedbackActivity extends CommonActivity {
+
+    @Override
+    Fragment getFragment() {
+        //TODO: return FeedbackFragment
+        return null;
+    }
+}
---
+++ app/src/main/java/com/vine/vinemars/app/fragment/FeedbackFragment.java
@@ -0,0 +1,33 @@
+package com.vine.vinemars.app.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by chengfei on 14/12/6.
+ */
+public class FeedbackFragment extends BaseFragment {
+
+    public static FeedbackFragment newInstance() {
+        return new FeedbackFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+}
