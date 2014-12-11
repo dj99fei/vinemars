349f09eef1a6fe6247d06c5d0dadc0119c0572c8
Fenn.Cheng <dj99fei@gmail.com>
2014-12-11 18:35:22 +0800

    add entry for feedback

--- app/src/main/java/com/vine/vinemars/app/FeedbackActivity.java
+++ app/src/main/java/com/vine/vinemars/app/FeedbackActivity.java
@@ -2,6 +2,8 @@ package com.vine.vinemars.app;
 
 import android.support.v4.app.Fragment;
 
+import com.vine.vinemars.app.fragment.FeedbackFragment;
+
 /**
  * Created by chengfei on 14/12/6.
  */
@@ -10,6 +12,6 @@ public class FeedbackActivity extends CommonActivity {
     @Override
     Fragment getFragment() {
         //TODO: return FeedbackFragment
-        return null;
+        return FeedbackFragment.newInstance();
     }
 }
